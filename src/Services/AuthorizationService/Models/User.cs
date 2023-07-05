abfa73f1440f96aceb53cc1d7a133bcce1764d63
S0dlive <sandlive662@gmail.com>
2023-07-05 12:25:25 +0200

    feat(auth-service): I have created the user model.

---
+++ src/Services/AuthorizationService/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthorizationService.Models;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string Description { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
