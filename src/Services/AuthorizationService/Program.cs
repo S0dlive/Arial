03b8c86649891666c22983dd8e5c567784682730
S0dlive <sandlive662@gmail.com>
2023-07-05 11:36:43 +0200

    feat(auth-service): I have implemented the cookie authentication for my authorization service

---
+++ src/Services/AuthorizationService/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
