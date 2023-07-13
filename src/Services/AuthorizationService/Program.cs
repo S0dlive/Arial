8a5e546a99587796ae84e4a094173bcd4d07bb30
lucia <sandlive662@gmail.com>
2023-07-13 12:03:28 +0200

    feat(auth-service): OpenIdDict some configuration for OAuth2.0 & a Client, Scope & Ressource Server creator.

--- src/Services/AuthorizationService/Program.cs
+++ src/Services/AuthorizationService/Program.cs
@@ -1,20 +1,41 @@
 using AuthorizationService.Data;
+using AuthorizationService.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHostedService<CreateTestClient>();
 
 builder.Services.AddDbContext<AuthorizationDbContext>(options =>
 {
     options.UseMySql("server=localhost;password=;user=root;database=arial.auth;"
         ,ServerVersion.AutoDetect("server=localhost;password=;user=root;database=arial.auth;"));
+
+    options.UseOpenIddict();
 });
+builder.Services.AddOpenIddict().AddCore(options =>
+{
+    options.UseEntityFrameworkCore().UseDbContext<AuthorizationDbContext>();
+}).AddServer(options =>
+{
+    options.AllowClientCredentialsFlow();
+    
+    options.AllowAuthorizationCodeFlow().RequireProofKeyForCodeExchange();
+    options.SetAuthorizationEndpointUris("auth/authorize");
+    options.SetIntrospectionEndpointUris("auth/introspect");
+    options.SetTokenEndpointUris("auth/token");
+    options.AddEphemeralEncryptionKey().AddEphemeralSigningKey();
+    options.RegisterScopes("api1");
+    options.UseAspNetCore().EnableTokenEndpointPassthrough().EnableAuthorizationEndpointPassthrough().DisableTransportSecurityRequirement();      
+}).AddValidation(options =>
+{ options.UseLocalServer(); options.UseAspNetCore(); });;
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
 {
     options.LoginPath = "/account/login";
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -26,10 +47,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-
+app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7262/"));
+app.UseHttpsRedirection();
 
 app.UseRouting();
 
---
+++ src/Services/AuthorizationService/Services/CreateTestClient.cs
@@ -0,0 +1,73 @@
+using AuthorizationService.Data;
+using OpenIddict.Abstractions;
+
+namespace AuthorizationService.Services;
+
+public class CreateTestClient : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CreateTestClient(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AuthorizationDbContext>();
+        await context.Database.EnsureCreatedAsync(cancellationToken);
+
+        var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+        if (await manager.FindByClientIdAsync("postman", cancellationToken) is null)
+        {
+            await manager.CreateAsync(new OpenIddictApplicationDescriptor
+            {
+                ClientId = "postman",
+                ClientSecret = "postman-secret",
+                DisplayName = "Postman",
+                RedirectUris = {new Uri("https://oauth.pstmn.io/v1/callback")},
+                Permissions =
+                {
+                    OpenIddictConstants.Permissions.Endpoints.Token,
+                    OpenIddictConstants.Permissions.GrantTypes.ClientCredentials,
+                    OpenIddictConstants.Permissions.Endpoints.Authorization,
+                    OpenIddictConstants.Permissions.Endpoints.Token,
+                    OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                    OpenIddictConstants.Permissions.ResponseTypes.Code,
+                    OpenIddictConstants.Permissions.ResponseTypes.Token,
+
+                    OpenIddictConstants.Permissions.Prefixes.Scope + "resource_server_1"
+                }
+            }, cancellationToken);
+        }
+        if (await manager.FindByClientIdAsync("resource_server_1") is null)
+        {
+            await manager.CreateAsync(new OpenIddictApplicationDescriptor
+            {
+                ClientId = "resource_server_1",
+                ClientSecret = "846B62D0-DEF9-4215-A99D-86E6B8DAB342",
+                Permissions =
+                {
+                    OpenIddictConstants.Permissions.Endpoints.Introspection
+                }
+            });
+        }
+        
+        var scopeManager = scope.ServiceProvider.GetRequiredService<IOpenIddictScopeManager>();
+        
+        if (await scopeManager.FindByNameAsync("resource_server_1") is null)
+        {
+            await scopeManager.CreateAsync(new OpenIddictScopeDescriptor
+            {
+                Name = "resource_server_1",
+                Resources =
+                {
+                    "resource_server_1"
+                },
+            });
+        }
+    }
+}
