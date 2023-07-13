d3ab52d277e3f1406ddba8458d5be6a0b049469c
lucia <sandlive662@gmail.com>
2023-07-13 11:58:17 +0200

    feat(auth-service): OAuth2.0 code flow was implemented.

--- src/Services/AuthorizationService/Controllers/AuthController.cs
+++ src/Services/AuthorizationService/Controllers/AuthController.cs
@@ -1,8 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
 
 namespace AuthorizationService.Controllers;
 
+[Route("auth")]
 public class AuthController : Controller
 {
+
+    [HttpPost("token")]
+    public async Task<IActionResult> Exchange()
+    {
+        var request = HttpContext.GetOpenIddictServerRequest() ?? throw new InvalidOperationException();
+
+        ClaimsPrincipal claimsPrincipal;
+
+        if (request.IsClientCredentialsGrantType())
+        {
+            var identity = new ClaimsIdentity();
+
+            identity.AddClaim(OpenIddictConstants.Claims.Subject,
+                request.ClientId ?? throw new InvalidOperationException());
+            identity.AddClaim("some-claim", "some-value", OpenIddictConstants.Destinations.AccessToken);
+            claimsPrincipal = new ClaimsPrincipal(identity);
+
+            claimsPrincipal.SetScopes(request.GetScopes());
+        }
+        else if (request.IsAuthorizationCodeGrantType())
+        {
+            claimsPrincipal = (await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+        
+        return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+    }
     
+    [HttpGet("authorize")]
+    [HttpPost("authorize")]
+    [IgnoreAntiforgeryToken]
+    public async Task<IActionResult> Authorize()
+    {
+        var request = HttpContext.GetOpenIddictServerRequest() ??
+                      throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+        
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        
+        if (!result.Succeeded)
+        {
+            return Challenge(
+                authenticationSchemes: CookieAuthenticationDefaults.AuthenticationScheme,
+                properties: new AuthenticationProperties
+                {
+                    RedirectUri = Request.PathBase + Request.Path + QueryString.Create(
+                        Request.HasFormContentType ? Request.Form.ToList() : Request.Query.ToList())
+                });
+        }
+        
+        var claims = new List<Claim>
+        {
+            new Claim(OpenIddictConstants.Claims.Subject, result.Principal.Identity.Name),
+            new Claim("some claim", "some value").SetDestinations(OpenIddictConstants.Destinations.AccessToken)
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        
+        claimsPrincipal.SetScopes(request.GetScopes());
+        var ticket = new AuthenticationTicket(
+            new ClaimsPrincipal(claimsIdentity),
+            new AuthenticationProperties(),
+            OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        ticket.Principal.SetResources(request.GetScopes().ToList());
+
+        return SignIn(ticket.Principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+    }
 }
