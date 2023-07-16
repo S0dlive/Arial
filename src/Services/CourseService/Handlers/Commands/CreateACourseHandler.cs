466285590608f97d45654613f87791b2baaf55cf
lucia <sandlive662@gmail.com>
2023-07-16 19:28:50 +0200

    feat(course-service): DeleteACourseHandler.cs

--- src/Services/CourseService/Handlers/Commands/CreateACourseHandler.cs
+++ src/Services/CourseService/Handlers/Commands/CreateACourseHandler.cs
@@ -1,12 +1,30 @@
 using CourseService.Commands;
+using CourseService.Data;
+using CourseService.Models;
 using MediatR;
 
 namespace CourseService.Handlers.Commands;
 
 public class CreateACourseHandler : IRequestHandler<CreateACourseCommand>
 {
-    public Task Handle(CreateACourseCommand request, CancellationToken cancellationToken)
+    private readonly CourseDbContext _courseDbContext;
+    public CreateACourseHandler(CourseDbContext courseDbContext)
     {
-        throw new NotImplementedException();
+        _courseDbContext = courseDbContext;
+    }
+    public async Task Handle(CreateACourseCommand request, CancellationToken cancellationToken)
+    {
+        Course course = new Course()
+        {
+            CourseName = request.CourseName,
+            CreatedAt = DateTime.Now,
+            Description = request.Description,
+            Id = Guid.NewGuid().ToString(),
+            LastUpdate = DateTime.Now,
+            OwnerId = request.OwnerId,
+            Price = request.Price,
+        };
+        _courseDbContext.Courses.Add(course);
+        await _courseDbContext.SaveChangesAsync();
     }
 }
