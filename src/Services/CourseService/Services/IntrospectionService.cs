9934d91dcf1d87eb0a880a6f336ee4973a85b92b
S0dlive <sandlive662@gmail.com>
2023-07-19 14:28:33 +0200

    feat(course-service): IntrospectionService.cs

---
+++ src/Services/CourseService/Services/IntrospectionService.cs
@@ -0,0 +1,29 @@
+﻿namespace CourseService.Services;
+
+public class IntrospectionService
+{
+    private readonly HttpClient _httpClient;
+    public IntrospectionService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetIdOfTheTokenByIntrospect(string token)
+    {
+        string introspectionEndpoint = "https://localhost:7262/introspect";
+        var requestContent = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("token", token),
+        });
+        
+        var response = await _httpClient.PostAsync(introspectionEndpoint, requestContent);
+        if (response.IsSuccessStatusCode)
+        {
+            throw new NotImplementedException();
+        }
+        else
+        {
+            return "THE REQUEST IS DOWN lol";
+        }
+    }
+}
