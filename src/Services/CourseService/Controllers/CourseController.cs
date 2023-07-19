d67d6d018bd23c3f9a813297b86dc06ac085f6aa
S0dlive <sandlive662@gmail.com>
2023-07-19 14:09:54 +0200

    feat(course-service): I have implemented the course controller.

--- src/Services/CourseService/Controllers/CourseController.cs
+++ src/Services/CourseService/Controllers/CourseController.cs
@@ -1,8 +1,34 @@
+using CourseService.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseService.Controllers;
 
+[ApiController()]
+[Route("api/course")]
 public class CourseController : Controller
 {
-    
+    private readonly IMediator _mediator;
+
+    public CourseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    public async Task<IActionResult> CreateCourseController(CreateACourseCommand createACourseCommand)
+    {
+        await _mediator.Send(createACourseCommand);
+        return Ok();
+    }
+
+    public async Task<IActionResult> UpdateCourseController(UpdateCourseCommand updateCourseCommand)
+    {
+        await _mediator.Send(updateCourseCommand);
+        return Ok();
+    }
+
+    public async Task<IActionResult> DeleteCourseController(DeleteCourseCommand deleteCourseCommand)
+    {
+        await _mediator.Send(deleteCourseCommand);
+        return Ok();
+    }
 }
