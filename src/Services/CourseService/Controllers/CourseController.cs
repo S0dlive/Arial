109a6991a631bb6bf2a6ca6c081ba6973b02f717
lucia <sandlive662@gmail.com>
2023-07-24 13:15:34 +0200

    feat(course-service): I have updated the course controller.

--- src/Services/CourseService/Controllers/CourseController.cs
+++ src/Services/CourseService/Controllers/CourseController.cs
@@ -45,10 +45,55 @@ public class CourseController : Controller
             await _courseDbContext.SaveChangesAsync();
             return Ok();
         }
-        return BadRequest("the sub is null. . .");
+        return BadRequest(new Error("400","the sub is null. . ."));
     }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetCoursesController([FromQuery]int limit)
+    {
+        if (limit > 100)
+        {
+            return BadRequest(new Error("400", "you don't can have more of 100 course."));
+        }
+
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{courseId}")]
+    [Authorize]
+    public async Task<IActionResult> UpdateAStatusCourse(string courseId, [FromBody] UpdateCourseRequest request)
+    {
+        var userId = User.Claims.FirstOrDefault(t => t.Type == "sub").Value;
+        var course = _courseDbContext.Courses.FirstOrDefault(t => t.Id == courseId);
+        if (course == null)
+        {
+            return NotFound(new Error("404", "this course doesn't exist."));
+        }
+
+        if (userId == null)
+        {
+            return BadRequest(new Error("400", "this token is not good"));
+        }
+        
+        if (course.OwnerId != userId)
+        {
+            return Unauthorized(new Error("401", "you don't have the authorization for update this course"));
+        }
+
+        course.CourseName = request.NewCourseName;
+        course.Description = request.NewDescription;
+        course.LastUpdate = DateTime.Now;
+        course.Price = request.NewPrice;
+
+        _courseDbContext.Courses.Update(course);
+        await _courseDbContext.SaveChangesAsync();
+        return Ok();
+    }
+    
+    
 }
 
+public record UpdateCourseRequest(string NewCourseName, double NewPrice, string NewDescription);
 public record CreateCourseRequest(string CourseName, double Price, string Description);
-
 public record Error(string ErrorStatusCode, string ErrorContent);
